2483e465bc420175490da77f6c94048838395a2b
Utibe Sampson <usampson3@gmail.com>
2024-08-20 15:12:16 +0100

    intro to javascript

---
+++ Javascript/script.js
@@ -0,0 +1,31 @@
+   // console.log("Hello World")
+
+        //Variables are containers used for holding objects like - var, let, const
+        
+        //String datatype
+        var fruit1 = 'Banana';
+        let fruit2 = "Orange";
+        const fruit3 = `Apple`;
+        let myCountry = 'Nigeria'
+        console.log(myCountry)
+
+        //Number datatype
+        var number1 = 10;
+        let number2 = 20/30;
+        const number3 = -30;
+
+        //Boolean datatype - a true or false value
+        var isRainy = true;
+        let isSunny = false;
+
+        //Undefined datatype - a variable that has not been assigned a value yet
+        let country;
+        //console.log(country)
+
+        //null datatype
+        country = null;
+        console.log(country);
+
+        //array
+        myCountry = 'England'
+        console.log(myCountry)
